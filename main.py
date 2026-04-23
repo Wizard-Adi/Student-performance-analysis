a9340579d90bf02f79fda9ac74b2538e2c65c01d
Aditya Kumar <adityakvs01@gmail.com>
2026-04-23 20:37:47 +0530

    fully functional preprocessing function

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.preprocess import preprocessing
+
+preprocessing()
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,16 @@
 import pandas as pd
 
 def preprocessing():
-    df = pd.read_csv("student_dataset_p2.csv")
+    path = "C:/Users/adity/OneDrive/Desktop/student_performance_analysis/data/student_dataset_p2.csv"
+    df = pd.read_csv(path)
 
+    # task 3.1
+    print("First 15 rows :\n", df.head(15))
+    #print("\nLast 10 rows :\n", df.tail(10))
+    print("\ndataset shape :\n", df.shape)
+    print("\nColumns\n", df.columns)
+
+    #task 3.2
     print("\nMIssing values\n",df.isnull().sum())
 
     df["Marks"] = df["Marks"].fillna(df["Marks"].mean())
@@ -10,3 +18,17 @@ def preprocessing():
     df["StudyHours"] = df["StudyHours"].fillna(df["StudyHours"].median())
 
     df.loc[df["StudyHours"] > 15, "StudyHours"] = df["StudyHours"].median()
+
+    df.loc[df["Marks"] > 100, "Marks"] = df["Marks"].median()
+
+    #task 3.3
+    df.loc[df["Marks"] >= 80, "Performance"] = "Excellent"
+
+    df.loc[(df["Marks"] >= 60) & (df["Marks"] <= 79), "Performance"] = "Good"
+
+    df.loc[df["Marks"] < 60, "Performance"] = "Needs Improvement"
+
+    df["EffortScore"] = df["StudyHours"] * df["Attendance"]
+
+    df.to_csv(path, index=False)
+    print("\nFile updated successfully\n")
