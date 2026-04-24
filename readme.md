116043616dd6f2ce38494c1bafc738baba281104
Aditya Kumar <adityakvs01@gmail.com>
2026-04-24 18:49:16 +0530

    Complete student performance analysis

---
+++ readme.md
@@ -0,0 +1,10 @@
+A - Insights
+1 - Study time doesn't change marks much: The correlation is -0.04, which is almost zero. This means studying more hours didn't always lead to higher marks in this data.
+2 - Too much study might be bad: Students who studied more than 10 hours had the lowest average marks (44.7). This could mean they are getting tired or "burnt out."
+3 - The "Best" study range: Students studying between 6 to 10 hours got the best average marks (69.6). This seems to be the perfect amount of time to study.
+4 - Attendance has a small impact: Average marks for "Satisfactory" and "Excellent" students are very similar (about 69 marks). This shows that higher attendance didn't significantly change the average score.
+5 - High passing rate: Most students in the dataset (1,086 out of 1,200) scored above 50 marks. This means the majority of students are performing at a passing level.
+
+B - Patterns Observed
+1 - The 10-Hour Drop: Marks are good for low and medium study hours, but they drop a lot once a student crosses 10 hours.
+2 - Steady Performance: Average marks stay nearly the same across all attendance categories, showing a very stable trend.
--- src/analysis.py
+++ src/analysis.py
@@ -23,3 +23,30 @@ def analysing():
 
     attendanceCorr = df["Attendance"].corr(df["Marks"])
     print(f"\nCorrelation coefficient Attendance vs Marks: {attendanceCorr:.2f}")
+
+    # group based analysis
+    df.loc[df["Attendance"] >= 90, "AttendanceCategory"] = "Excellent"
+
+    df.loc[(df["Attendance"] >= 75) & (df["Attendance"] <= 89), "AttendanceCategory"] = "Good"
+
+    df.loc[(df["Attendance"] >= 50) & (df["Attendance"] <= 74), "AttendanceCategory"] = "Satisfactory"
+
+    df.loc[df["Attendance"] < 50, "AttendanceCategory"] = "Poor"
+
+    df.to_csv(location, index=False)
+    print("\nFile updated successfully\n")
+
+    # Group by attendance and calculate the mean of the marks column
+    avg_marks_by_attendance = df.groupby('AttendanceCategory')['Marks'].mean()
+
+    print("\nAverage marks based on different Attendance levels : ",avg_marks_by_attendance)
+
+    #group by study category then mean()
+    # 1. Create attendance groups (bins)
+    bins = [-1, 5, 10, 15]
+    labels = ['Low (0-5)', 'Medium (6-10)', 'High (>10)']
+    df["StudyHoursGroup"] = pd.cut(df["StudyHours"], bins=bins, labels=labels)
+
+    # 2. Find average marks for these new groups
+    result = df.groupby("StudyHoursGroup", observed=False)["Marks"].mean()
+    print("\nAverage marks based on different Study Hours : ",result)
